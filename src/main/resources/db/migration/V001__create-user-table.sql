cc3c0da873564740338c86efcb600f37fd575177
denilson <denilson.lima@petzpartner.com.br>
2024-03-09 15:18:27 -0300

    feat: setup database environment using Docker

---
+++ src/main/resources/db/migration/V001__create-user-table.sql
@@ -0,0 +1,8 @@
+create table users (
+    id serial PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    date_of_birth DATE,
+    cpf VARCHAR(11) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    UNIQUE(cpf, email)
+)
